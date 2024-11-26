7d35a9715b838f6914513d014ed8eec081b93a8f
GertenP <gerten.pilv@tptlive.ee>
2024-11-26 14:53:48 +0200

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,55 @@
 import { useState } from 'react'
+import './App.css';
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
   return (
-    <div>
-      <div>
-        <h2>Give feedback</h2>
-
-        <button onClick={() => { setGood(good + 1) }}>good</button>
-        <button onClick={() => { setNeutral(neutral + 1) }}>neutral</button>
-        <button onClick={() => { setBad(bad + 1) }}>bad</button>
+    <div className="app-container">
+      <div id="feedback-section" className="box">
+        <h1>Give feedback</h1>
+        <div className="buttons">
+          <button onClick={() => { setGood(good + 1) }}>good</button>
+          <button onClick={() => { setNeutral(neutral + 1) }}>neutral</button>
+          <button onClick={() => { setBad(bad + 1) }}>bad</button>
+        </div>
       </div>
 
-      <div>
-        <h2>Statistics</h2>
+      <div id="statistics-section" className="box">
+        <h1>Statistics</h1>
         <table>
-          <tr>
-            <td>Good</td>
-            <td>{good}</td>
-          </tr>
-          <tr>
-            <td>neutral</td>
-            <td>{neutral}</td>
-          </tr>
-          <tr>
-            <td>bad</td>
-            <td>{bad}</td>
-          </tr>
-          <tr>
-            <td>all</td>
-            <td>{bad + neutral + good}</td>
-          </tr>
-          <tr>
-            <td>average</td>
-            <td>{(good*1 + neutral*0 + bad*-1) / (bad + neutral + good)}</td>
-          </tr>
-          <tr>
-            <td>positive</td>
-            <td>{Math.round(good / (bad + neutral + good) * 100)}%</td>
-          </tr>       
-          </table>
-
+          <tbody>
+            <tr>
+              <td>Good</td>
+              <td>{good}</td>
+            </tr>
+            <tr>
+              <td>Neutral</td>
+              <td>{neutral}</td>
+            </tr>
+            <tr>
+              <td>Bad</td>
+              <td>{bad}</td>
+            </tr>
+            <tr>
+              <td>All</td>
+              <td>{bad + neutral + good}</td>
+            </tr>
+            <tr>
+              <td>Average</td>
+              <td>{(good * 1 + neutral * 0 + bad * -1) / (bad + neutral + good)}</td>
+            </tr>
+            <tr>
+              <td>Positive</td>
+              <td>{Math.round(good / (bad + neutral + good) * 100)}%</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-
     </div>
-
   )
 }
 
-export default App
+export default App;
